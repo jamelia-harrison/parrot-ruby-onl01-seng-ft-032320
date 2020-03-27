6c001f936717da1c7172a8bbf5e3289b15564fb9
jamelia harrison <jameliaharrison@gmail.com>
2020-03-27 08:27:38 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (speak="Squawk")
- puts "I have a long beak. When I'm happy I #{speak}!"
- "I have a long beak. When I'm happy I #{speak}!"
+
+def parrot(phrase = "Squawk!")
+  puts "#{phrase}"
+  return phrase
 end
-parrot
